46fb0237ce4e84c08451be0a6b14edb17eddd161
salah <selqaddoury01@educarex.es>
2025-06-05 14:55:01 +0200

    .

--- APIbiblioteca/app/Http/Controllers/PrestamoController.php
+++ APIbiblioteca/app/Http/Controllers/PrestamoController.php
@@ -68,10 +68,13 @@ class PrestamoController extends Controller
                 return response()->json(['error' => 'El socio no existe'], 400);
             }
 
-            // verificar si tiene multa pendiente
-            $multa = Multa::where('id_usuario', $request->id_socio)->where('pagada', false)->first();
+
+            $multa = Multa::where('id_usuario', $request->id_socio)
+                ->where('pagada', false) // o 0, según el tipo de dato
+                ->first();
+
             if ($multa) {
-                return response()->json(['error' => 'Tienes multas pendientes'], 400);
+                return response()->json(['error' => 'Tienes multas pendientes y no puedes reservar.'], 403);
             }
 
             DB::beginTransaction();
